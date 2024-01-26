f9be0249581c9ca71ef68d2a14b3d1e684d8e4c6
ParvinMusayev <parvinmusayev1@gmail.com>
2024-01-27 00:12:40 +0400

    fix: Render Function

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -18,10 +18,7 @@ const createPhoto = async (req, res) => {
 const getAllPhotos = async (req, res) => {
   try {
     const photos = await Photo.find({});
-    res.status(200).json({
-      succeded: true,
-      photos,
-    });
+    res.status(200).render("photos")
   } catch (error) {
     res.status(500).json({
       succeded: false,
