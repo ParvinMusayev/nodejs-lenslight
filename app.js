71447c5b134ce1013fc26e5555865a12b34816e2
ParvinMusayev <parvinmusayev1@gmail.com>
2024-01-26 21:48:32 +0400

    feat: Add photoController and photoRoute

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import conn from "./db.js";
 
 import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(express.static("public"));
 
 //routes
 app.use("/", pageRoute);
+app.use("/photos", photoRoute);
 
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
