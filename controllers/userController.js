35817936f73ded2df05290287ba5233c41e93e44
ParvinMusayev <parvinmusayev1@gmail.com>
2024-01-28 23:30:20 +0400

    feat: Add cookie-parser

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,10 +35,16 @@ const loginUser = async (req, res) => {
     }
 
     if(same){
-      res.status(200).json({
-        user,
-        token: createToken(user._id)
-      })
+
+      const token =  createToken(user._id)
+      res.cookie('jwt', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24,
+      });
+
+      res.redirect('/users/dashboard');
+
+     
     } else {
       res.status(401).json({
         succeded: false,
