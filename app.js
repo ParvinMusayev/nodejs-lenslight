8dd69435edbd9416234dfb239a714bf35449f92b
ParvinMusayev <parvinmusayev1@gmail.com>
2024-01-29 12:10:59 +0400

    feat: Add checkUser function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import pageRoute from "./routes/pageRoute.js";
 import photoRoute from "./routes/photoRoute.js";
 import userRoute from "./routes/userRoute.js";
+import { checkUser } from "./middlewares/authMiddleware.js";
 
 dotenv.config();
 
@@ -21,14 +22,14 @@ app.set("view engine", "ejs");
 //static files middleware
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 //routes
+app.get("*", checkUser)
 app.use("/", pageRoute);
 app.use("/photos", photoRoute);
 app.use("/users", userRoute);
-
 
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
