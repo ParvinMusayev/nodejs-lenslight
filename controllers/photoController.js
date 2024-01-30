940d3f28ddffbd698ac4b0e2558f9655329310a1
ParvinMusayev <parvinmusayev1@gmail.com>
2024-01-30 23:30:20 +0400

    chore: uptade photo page

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,12 +1,13 @@
 import Photo from "../models/photoModel.js";
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 const createPhoto = async (req, res) => {
   const result = await cloudinary.uploader.upload(
     req.files.image.tempFilePath,
     {
       use_filename: true,
-      folder: 'lenslight',
+      folder: "lenslight",
     }
   );
 
@@ -15,9 +16,12 @@ const createPhoto = async (req, res) => {
       name: req.body.name,
       description: req.body.description,
       user: res.locals.user._id,
-      
+      url: result.secure_url,
     });
-    res.status(201).redirect("/users/dashboard")
+
+    fs.unlinkSync(req.files.image.tempFilePath);
+
+    res.status(201).redirect("/users/dashboard");
   } catch (error) {
     res.status(500).json({
       succeded: false,
@@ -43,7 +47,7 @@ const getAllPhotos = async (req, res) => {
 
 const getAPhotos = async (req, res) => {
   try {
-    const photo = await Photo.findById({ _id: req.params.id });
+    const photo = await Photo.findById({ _id: req.params.id }).populate("user")
     res.status(200).render("photo", {
       photo,
       link: "photos",
